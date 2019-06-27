fe03e3e22a5c8e8f7f60fb2e4acb52f4131e1aa3
K.saranya <51949493+saranya258@users.noreply.github.com>
2019-06-27 11:01:46 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+char=input()
+if char.isalpha():
+    if(char=='A'or char=='E'or char=='I'or char=='O'or char=='U'or char=='a'or char=='e'or char=='i'or char=='o'or char=='u'):
+          print("Vowel")
+    else:
+          print("Consonant")
+else:
+    print("invalid")
